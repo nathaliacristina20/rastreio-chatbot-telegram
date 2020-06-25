76d735f1a62c7f520452885ac2bec68d0bc49324
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-06-24 23:20:29 -0300

    format date with date-fns

--- index.js
+++ index.js
@@ -1,63 +1,98 @@
 const TelegramBot = require("node-telegram-bot-api");
+const { rastro } = require("rastrojs");
 
-require('dotenv').config();
+const DateFns = require("date-fns");
+
+const ptBR = require("date-fns/locale/pt-BR");
+
+require("dotenv").config();
 
 const token = process.env.TOKEN;
 
-const { rastro } = require("rastrojs");
-
 const bot = new TelegramBot(token, { polling: true });
+
+let modeTrack = false;
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
 
   var myRe = /^[a-z,A-Z]{2}[0-9]{9}[a-z,A-Z]{2}$/;
   var start = /\/start/;
+  
+  if (
+    start.exec(msg.text.toString()) ||
+    ["oi", "ola", "hi"].includes(msg.text.toString().toLowerCase())
+  ) {
 
-  if (start.exec(msg.text.toString())) {
+    modeTrack = false;
+
     bot.sendMessage(
       msg.chat.id,
-      `Olá ${msg.from.first_name}, digite seu código para rastrear a sua encomenda.`
-    );
-  } else if (["oi", "ola", "hi"].includes(msg.text.toString().toLowerCase())) {
-    bot.sendMessage(
-      msg.chat.id,
-      `Olá ${msg.from.first_name}, digite seu código para rastrear a sua encomenda.`
+      `Olá ${msg.from.first_name}, seja benvindo(a)! Como posso te ajudar hoje?`,
+      {
+        reply_markup: {
+          keyboard: [["Rastrear minha encomenda"]],
+        },
+      }
     );
   } else if (
     ["tchau", "flw", "ate", "ate mais"].includes(
       msg.text.toString().toLowerCase()
     )
   ) {
+
+    modeTrack = false;
+    
     bot.sendMessage(msg.chat.id, "Espero ver você novamente! Até breve.");
-  } else if (myRe.exec(msg.text.toString().toUpperCase())) {
+  } else if (msg.text.toString() === "Rastrear minha encomenda") {
+
+    modeTrack = true;
+
+    bot.sendMessage(
+      msg.chat.id,
+      "Certo! Digite seu código para rastreiar sua encomenda."
+    );
    
-    rastro
-      .track(msg.text.toString().toUpperCase())
-      .then((track) => {
+  } else if (modeTrack === true){
 
-        if (track[0].isInvalid){
-          bot.sendMessage(msg.chat.id, "Humm, parece que o código que você digitou não é válido. Confira e tente novamente!");
-        } else {
+    if (myRe.exec(msg.text.toString().toUpperCase())) {
+      rastro
+        .track(msg.text.toString().toUpperCase())
+        .then((track) => {
+          if (track[0].isInvalid) {
+            bot.sendMessage(
+              msg.chat.id,
+              "Humm, parece que o código que você digitou não é válido. Confira e tente novamente!"
+            );
+          } else {
+            const dateFormatted = DateFns.format(
+              track[0].tracks[track[0].tracks.length - 1].trackedAt,
+              "dd 'de' MMMM 'de' yyyy 'às' HH:mm",
+              { locale: ptBR }
+            );
 
-          let msg = `${track[0].tracks[track[0].tracks.length - 1].locale}\n`;
-          msg += `${track[0].tracks[track[0].tracks.length - 1].status}\n`;
-          msg += `${track[0].tracks[track[0].tracks.length - 1].observation}`;
-  
-          bot.sendMessage(chatId, msg);
+            console.log(dateFormatted);
+            let msg = `${dateFormatted}\n`;
+            msg += `${track[0].tracks[track[0].tracks.length - 1].locale}\n`;
+            msg += `${track[0].tracks[track[0].tracks.length - 1].status}\n`;
+            msg += `${track[0].tracks[track[0].tracks.length - 1].observation}`;
 
-        }
-      
-      })
-      .catch((error) => {
-        console.log(error);
-        bot.sendMessage(chatId, "Ops, algo deu errado. Tente novamente!");
-      });
+            bot.sendMessage(chatId, "Este é o último status da sua encomenda");
+            bot.sendMessage(chatId, msg);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          bot.sendMessage(chatId, "Ops, algo deu errado. Tente novamente!");
+        });
+    } else {
+      bot.sendMessage(chatId, "Humm, parece que o código que você digitou não é válido. Confira e tente novamente!");
+    }
+    
   } else {
     bot.sendMessage(
       msg.chat.id,
       "Ops, desculpe! Não entendi o que você quis dizer."
     );
   }
-
 });
